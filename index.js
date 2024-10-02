b95358a7eb3be1ef3499ad5367adda0c35ea432d
Viswa Harini Vidyashankar <viswavidya16@gmail.com>
2024-10-02 12:06:05 -0400

    add post route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import videos from './routes/videos.js'
 
 const app = express();
 app.use(cors());
@@ -8,10 +9,19 @@ app.use(cors());
 const PORT = 8080;
 
 app.get('/', function(req, res) {
-    res.send('server setup');
+    res.send('hi');
 })
 
 
-app.listen(PORT, function() {
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+})
+
+app.use('/videos', videos)
+
+app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
 })
---
+++ routes/videos.js
@@ -0,0 +1,89 @@
+import fs from 'fs';
+import express from 'express'
+import crypto from "crypto";
+
+
+const router = express.Router();
+
+const getVideos = () => {
+    const data = fs.readFileSync('./data/videos.json');
+    const parsedData = JSON.parse(data);
+    return parsedData;
+}
+
+router.get('/', (req, res) => {
+    const videos = getVideos();
+    if(!videos.length) {
+        res.status(404).send('The request does not exists')
+    }
+    res.status(200).json(videos);
+})
+
+router.get('/:id', (req, res) => {
+    const videos = getVideos();
+    const selectedVideo = videos.find((video) => video.id === req.params.id )
+
+    if(selectedVideo) {
+        res.status(200).json(selectedVideo);
+    }
+    else {
+        res.status(404).send('The requested video does not exists')
+    }
+})
+
+const channelNames = ['Cornelia Currey', 'Dalia Bennu', 'Emmett Wilson', 'Album Art Exchange', 'Record Room', 'Discography Deep Dive' ];
+
+const imageArray = [];
+
+const viewsArray = ['345,236', '245,334', '34,589', '18,453', '85,317', '67,132'];
+
+const likesArray = ['36,481', '72,278', '381,234', '520,337', '165,330', '92,115'];
+
+const durationArray = ['32:12', "14:45", '12:30', '22:50', '40:10', '25:40'];
+
+const timestampArray = [1676371200000, 1672531200000, 1681116000000, 1685010300000, 1688169599000];
+
+const commentersArray = ['Olivia Davis', 'Ethan Garcia', 'Ava Wilson', 'Mia Robinson', 'Isabella Taylor'];
+
+const commentsArray = ['The collaborations on this album are fantastic; they really elevate the songs.', 'I cant get enough of the melodies—so catchy and memorable!', 'I always look forward to your uploads. Never disappoints!', 'I cant believe I just discovered your channel! Subscribed!', 'This album is a masterpiece! The production quality is top-notch.']
+
+const commentDates = [1693526400000, 1696099199000, 1696924800000, 1698796800000, 1699957800000];
+
+const getRandomItem = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+router.post('/', (req, res) => {
+
+
+    const videoInfo = {
+        id: crypto.randomUUID(),
+        title: req.body.title,
+        description: req.body.description,
+        channel: getRandomItem(channelNames),
+        image: "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg",
+        views: getRandomItem(viewsArray),
+        likes: getRandomItem(likesArray),
+        duration: getRandomItem(durationArray),
+        video: "nil",
+        timestamp: getRandomItem(timestampArray),
+        comments: [
+            {
+                id: crypto.randomUUID(),
+                name: getRandomItem(commentersArray),
+                comment: getRandomItem(commentsArray),
+                likes: Math.floor(Math.random() * 5),
+                timestamp: getRandomItem(commentDates)
+            }
+        ]
+
+    }
+    const videos = getVideos();
+    videos.push(videoInfo);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+
+    res.status(201).json(videoInfo);
+})
+
+export default router;
